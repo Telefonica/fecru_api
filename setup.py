1133a53f3b054b4b21badc930b3a97efa79b0c06
Taras Sotnikov <taras.sotnikov@telefonica.com>
2023-01-03 17:11:49 +0100

    Update build to publish package to nexus

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
-from setuptools import setup, find_packages
-import sys, os
+import setuptools
 
 version = '0.1'
 
-setup(name='fecru_api',
-      version=version,
-      description="Small wrapper for fecru REST API.",
-      long_description="""\
+setuptools.setup(name='fecru_api',
+    version=version,
+    description="Small wrapper for fecru REST API.",
+    long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='atlassian fecru fisheye crucible',
-      author='Jose Plana',
-      author_email='jplana@tuenti.com',
-      url='',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=["simplejson",
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords='atlassian fecru fisheye crucible',
+    author='InfraDX Team',
+    author_email='novum-infradx@telefonica.com',
+    url='https://github.com/Telefonica/fecru_api',
+    packages=setuptools.find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=True,
+    python_requires=">=3.7",
+    install_requires=[
+       "simplejson",
+    ],
+)
